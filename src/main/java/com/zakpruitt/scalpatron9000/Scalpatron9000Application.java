7094f68fbe8acb682a1438155ad3aff31f14c375
Zak Pruitt <pruittzn@gmail.com>
2025-04-19 01:50:28 -0500

    init repo

---
+++ src/main/java/com/zakpruitt/scalpatron9000/Scalpatron9000Application.java
@@ -0,0 +1,13 @@
+package com.zakpruitt.scalpatron9000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Scalpatron9000Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Scalpatron9000Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/zakpruitt/scalpatron9000/Scalpatron9000ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zakpruitt.scalpatron9000;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Scalpatron9000ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
